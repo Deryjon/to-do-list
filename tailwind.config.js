1b5319375802cfa38d623150198220e6894e3263
lonewolf0844 <lonewolf.6120844@gmail.com>
2023-05-13 17:13:21 +0500

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,16 @@ export default {
   ],
   theme: {
     extend: {
-			colors: {
-				"light": "F1F6F9",
-				"blue": "394867",
-				"dark-blue": "212A3E",
-				"gray": "9BA4B5",
-			}
-		},
-		container: {
-			center: true
-		}
+      colors: {
+        'light': '#F1F6F9',
+        'blue': '#394867',
+        'dark-blue': '#212A3E',
+        'gray': '#9BA4B5'
+      }
+    },
+    container: {
+      center: true  
+    }
   },
   plugins: [],
 }
